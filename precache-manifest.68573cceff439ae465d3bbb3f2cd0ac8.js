a547ca956937b619abe8765ce076e5e9f0568fbf
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-06 15:22:09 -0700

    Updates

---
+++ precache-manifest.68573cceff439ae465d3bbb3f2cd0ac8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a142ea2e6ed5f01419eb905f5627e2",
+    "url": "/React-counter-app/index.html"
+  },
+  {
+    "revision": "1cf5085438743d14f46d",
+    "url": "/React-counter-app/static/css/2.49e2ef92.chunk.css"
+  },
+  {
+    "revision": "26636427abd61fe818cd",
+    "url": "/React-counter-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1cf5085438743d14f46d",
+    "url": "/React-counter-app/static/js/2.15e6d668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-counter-app/static/js/2.15e6d668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26636427abd61fe818cd",
+    "url": "/React-counter-app/static/js/main.7e9970c5.chunk.js"
+  },
+  {
+    "revision": "ea158f7e20c81d2a9c60",
+    "url": "/React-counter-app/static/js/runtime-main.c79979d5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
